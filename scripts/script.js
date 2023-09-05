2410fede40374ae4c9770361ed57c3d7b95dfe4f
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-05 15:25:45 +0500

    added method forEach

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,11 @@
 // const numbers = [1, 2, 3, 4, 5];
 // console.log(numbers);
+
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach(test);
+function test(item, index, array) {
+  console.log(item);
+  console.log(index);
+  console.log(array);
+}
