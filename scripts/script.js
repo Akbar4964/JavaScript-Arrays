92b2ef02fef51778eea444d3845c1985b78ebbe1
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-05 16:10:57 +0500

    added method Map

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,28 @@
 //   console.log(index);
 //   console.log(array);
 // }
+
+// const numbers = [
+//   {
+//     name: "Iphone15",
+//     price: 1500,
+//     count: 3,
+//   },
+//   {
+//     name: "Iphone14",
+//     price: 1400,
+//     count: 13,
+//   },
+//   {
+//     name: "Iphone13",
+//     price: 1300,
+//     count: 7,
+//   },
+// ];
+// const numbersValues = numbers.map((numbers) => ({
+//   telModel: numbers.name,
+//   realPrice: numbers.price,
+//   allPrice: numbers.price * numbers.count,
+//   howMuch: numbers.count,
+// }));
+// console.log(numbersValues);
