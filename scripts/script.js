742f916171b1dcce28ac322be00736f4c704acc6
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-11 16:25:15 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -368,3 +368,7 @@
 // const numbers = [1, 2, 3, 4, 5];
 // numbers.push(" Shuhratullayev");
 // console.log(numbers);
+
+// const numbers = [1, 2, 3, 4, 5];
+// numbers.shift();
+// console.log(numbers);
